dc3ee4ec7f246b85496fb7df4b7d0478dfd2c646
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-08 12:11:54 +0300

    build functio fix.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -43,7 +43,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname+ '/build'),
+    path: path.resolve('./build'),
     filename: 'index.js',
     libraryTarget: 'umd',
   },
